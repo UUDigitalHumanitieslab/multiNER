591fbad4b32e24125f1815ffd5663709057d7460
Alex Hebing <a.h.j.hebing@uu.nl>
2019-04-26 10:51:15 +0200

    Polyglot refactored for NamedEntities and tests added

--- ner_packages/polyglot.py
+++ ner_packages/polyglot.py
@@ -3,23 +3,15 @@ import ast
 import threading
 from polyglot.text import Text
 
-'''
-PER ORG LOC
-'''
+from .named_entity import NamedEntity
+
+
 class Polyglot(threading.Thread):
     '''
         Wrapper for Polyglot.
 
         http://polyglot.readthedocs.io/en/latest/index.html
 
-        >>> test = "Deze iets langere test bevat de naam Einstein."
-        >>> p = Polyglot(parsed_text=test)
-        >>> p.start()
-        >>> import time
-        >>> time.sleep(0.1)
-        >>> from pprint import pprint
-        >>> pprint(p.join())
-        {'polyglot': [{'ne': 'Einstein', 'pos': 37, 'type': 'person'}]}
     '''
 
     def __init__(self, language='en', text_input=''):
@@ -29,47 +21,54 @@ class Polyglot(threading.Thread):
 
 
     def run(self):
-        buffer_all = []
+        data = self.collect_data()
+        polyglot_entities = self.parse_response(data)
+        self.result = self.convert(polyglot_entities)
+        
 
+    def collect_data(self):
         try:
-            text = Text(self.text_input, hint_language_code=self.language)
-
-            for sent in text.sentences:
-                entity_buffer = []
-                prev_entity_start = -1
-                for entity in sent.entities:
-                    if entity not in entity_buffer:
-                        # For some reason polyglot double's it's output.
-                        if not prev_entity_start == entity.start:
-                            prev_entity_start = entity.start
-                            entity_buffer.append(entity)
-                        else:
-                            entity_buffer.pop()
-                            entity_buffer.append(entity)
-
-                for item in entity_buffer:
-                    buffer_all.append(item)
-
-            result = []
-            for entity in buffer_all:
-                # For there is no sane way to do this.
-                ne = " ".join(ast.literal_eval(entity.__str__()))
-                tag = str(entity.tag.split('-')[1])
-                result.append({"ne": ne,
-                               "type": tag})
-
-            offset = 0
-            for i, ne in enumerate(result):
-                ne = ne["ne"]
-                pos = self.text_input[offset:].find(ne)
-                result[i]["pos"] = pos + offset
-                offset += pos + len(ne)
+            return Text(self.text_input, hint_language_code=self.language)
         except Exception as e:
             print('polyglot error:')
             print(e)
-            result = []
 
-        self.result = {"polyglot": result}
+    
+    def parse_response(self, data):
+        entities = []
+
+        for entity in data.entities:
+            entities.append({'text': entity._collection[0], 'type': self.parse_type(entity.tag)})
+
+        return entities
+
+    
+    '''
+    Calculate position and create NamedEntity instances
+    '''
+    def convert(self, polyglot_entities):
+        named_entities = []
+        
+        offset = 0
+        for pge in polyglot_entities:
+            text = pge["text"]
+            pos = self.text_input[offset:].find(text)
+            position = pos + offset
+            offset += pos + len(text)
+            
+            ne = NamedEntity(text, "polyglot", position, pge['type'])
+            named_entities.append(ne)
+        
+        return named_entities
+
+    
+    def parse_type(self, polyglot_type):
+        if polyglot_type == 'I-PER':
+            return 'PERSON'
+        if polyglot_type == 'I-LOC':
+            return 'LOCATION'
+        if polyglot_type == 'I-ORG':
+            return 'ORGANIZATION'
 
 
     def join(self):
