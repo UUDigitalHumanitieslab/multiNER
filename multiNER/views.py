6fc471e6b0e1c925b1fff978aed184ec99ad6076
Alex Hebing <a.h.j.hebing@uu.nl>
2019-05-08 10:59:09 +0200

    Validation for configuration added

--- multiNER/views.py
+++ multiNER/views.py
@@ -7,7 +7,6 @@ from flask import request, Response, current_app, Blueprint, render_template, fl
 from .forms import NerForm
 from .ner import MultiNER, Configuration
 
-
 bp = Blueprint('ner', __name__, url_prefix='/ner')
 
 
@@ -15,21 +14,20 @@ bp = Blueprint('ner', __name__, url_prefix='/ner')
 def index():
     form = NerForm()
     if form.validate_on_submit():
-        
+
         text = form.text.data.replace('\r', ' ').replace('\n', ' ')
-        
+
         ner_config = Configuration(
-            form.language.data, 
-            5, 
-            ['stanford', 'spotlight'], 
-            2, 
+            form.language.data,
+            5,
+            ['stanford', 'spotlight'],
+            2,
             {1: 'LOCATION', 2: 'PERSON', 3: 'ORGANIZATION', 4: 'OTHER'})
-        
-    
-        multiner = MultiNER(current_app.config, ner_config)        
+
+        multiner = MultiNER(current_app.config, ner_config)
         input = {'title': form.title.data, 'text': text}
         entities_per_part = multiner.find_entities(input)
-        
+
         return render_template('multiNER/results.html', text=text, entities=entities_per_part)
 
     return render_template('multiNER/index.html', form=form)
@@ -54,13 +52,14 @@ def collect_from_text():
     else:
         title = jsonData['title']
 
-    ner_config = get_configuration(jsonData['configuration'])
-    
+    validated_config = validate_configuration(jsonData['configuration'])
+    ner_config = get_configuration(validated_config)
+
     multiner = MultiNER(current_app.config, ner_config)
-    
+
     input = {'title': title, 'text': text}
     entities_per_part = multiner.find_entities(input)
-    
+
     resp = Response(response=json.dumps(entities_per_part),
                     mimetype='application/json; charset=utf-8')
     return (resp)
@@ -73,12 +72,12 @@ def get_configuration(config):
     if not 'context_length' in config:
         config['context_length'] = 5
 
-    if not 'leading_packages' in config:
-        config['leading_packages'] = ['stanford', 'spotlight']
+    if not 'leading_ner_packages' in config:
+        config['leading_ner_packages'] = ['stanford', 'spotlight']
 
     if not 'other_packages_min' in config:
         config['other_packages_min'] = 2
-    
+
     if 'type_preference' in config:
         converted_keys = {}
         for key, type in config['type_preference'].items():
@@ -86,12 +85,53 @@ def get_configuration(config):
 
         config['type_preference'] = converted_keys
     else:
-        config['type_preference'] = {1: 'LOCATION',
-                                     2: 'PERSON', 3: 'ORGANIZATION', 4: 'OTHER'}
+        config['type_preference'] = {
+            1: 'LOCATION',
+            2: 'PERSON',
+            3: 'ORGANIZATION',
+            4: 'OTHER'
+        }
 
     return Configuration(
-        config['language'], 
-        config['context_length'], 
-        config['leading_packages'], 
-        config['other_packages_min'], 
+        config['language'],
+        config['context_length'],
+        config['leading_ner_packages'],
+        config['other_packages_min'],
         config['type_preference'])
+
+
+def validate_configuration(config):
+    if 'language' in config:
+        if not config['language'] in ['en', 'it', 'nl']:
+            abort(400, "language '{}' is not supported".format(
+                config['language']))
+
+    if 'context_length' in config:
+        if not isinstance(config['context_length'], int):
+            abort(400, "context_length should be an integer")
+        if 0 > config['context_length'] > 15:
+            abort(400, "context_length should be a number between 0 and 15")
+
+    if 'leading_ner_packages' in config:
+        print('hello')
+        
+        for package in config['leading_ner_packages']:
+            if not package in ['stanford', 'spotlight', 'spacy', 'polyglot']:
+                abort(400, "package '{}' is not supported".format(package))
+
+    if 'other_packages_min' in config:
+        if 1 > config['other_packages_min'] or config['other_packages_min'] > 4:
+            abort(400, "other_packages_min should be a number between 1 and 4")
+
+    if 'type_preference' in config:
+        if not isinstance(config['type_preference'], dict):
+            abort(400, "type_preference should be a dictionary")
+
+        for key, value in config['type_preference']:
+            if not key in ['1', '2', '3', '4']:
+                abort(400, "Key '{}' is invalid in type_preference".format(key))
+
+            if not value in ['LOCATION', 'PERSON', 'ORGANIZATION', 'OTHER']:
+                abort(400, "Value '{}' is invalid in type_preference".format(value))
+
+    return config
